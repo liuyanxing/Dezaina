757233681be69baf33190d963466ea0932f4d258
lyx <1464062049@qq.com>
2023-04-05 22:52:51 +0800

    refract

--- src/core/source/function/nodes/meta/const.ts
+++ src/core/source/function/nodes/meta/const.ts
@@ -1,6 +1,6 @@
 import path from "path";
 export const outDir = path.join(__dirname, "out");
-export const nodeIncludeDir = path.join(__dirname, "../pre_include");
+export const nodeIncludeDir = path.join(__dirname, "../include_gen");
 export const schemaFileName = "desaina.kiwi";
 export const schemaHppFileName = "desaina_node_kiwi.h";
 export const desainaHppFileName = "desaina_node.h";
