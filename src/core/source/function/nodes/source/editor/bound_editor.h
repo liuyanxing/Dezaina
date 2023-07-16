1f3ceed8096f164174994194559aeec685e1598b
liuyanxing <lyx2049@qq.com>
2023-07-16 19:57:26 +0800

    add

--- src/core/source/function/nodes/source/editor/bound_editor.h
+++ src/core/source/function/nodes/source/editor/bound_editor.h
@@ -1,4 +1,5 @@
 #include "editor.h"
+#include <vector>
 
 enum class ControlNodeType {
 	kResize,
@@ -11,8 +12,20 @@ struct ControlNode : public HitTestNode {
 };
 
 
-class BoundEditor : public Editor {
+class SelectBoundEditor : public Editor {
+public:
+	SelectBoundEditor(Document* document) {
+		document_ = document;
+		init();
+	};
 	void init() {
+		// add control nodes
+		auto selections = document_.getSelections();
+		SkRect bound = SkRect::MakeEmpty();
+		for (auto& selection : selections) {
+			bound.join(selection->getBound());
+		}
+		auto selectionBound = document_->getClientBoundFromWorldBound(bound);
 		mouseEventEmmiter_.addHitTestNode(HitTestNode::Make(0, SkRect::MakeXYWH(0, 0, 100, 100)));
 		 mouseEventEmitter_.addEventListener<MouseEvent>([this](MouseEvent event) {
 			 if (event.type == MouseEvent::Type::kDown) {
@@ -23,4 +36,20 @@ class BoundEditor : public Editor {
 			 }
 		 });
 	}
+	void build() {
+		// update control nodes
+		auto selections = document_.getSelections();
+		SkRect bound = SkRect::MakeEmpty();
+		for (auto& selection : selections) {
+			bound.join(selection->getBound());
+		}
+		auto selectionBound = document_->getClientBoundFromWorldBound(bound);
+		ctrl_nodes_.clear();
+		ctrl_nodes_.push_back(ControlNode::Make(ControlNodeType::kResize, SkRect::MakeXYWH(selectionBound.fRight - 10, selectionBound.fBottom - 10, 10, 10)));
+		ctrl_nodes_.push_back(ControlNode::Make(ControlNodeType::kKResize, SkRect::MakeXYWH(selectionBound.fRight - 10, selectionBound.fTop, 10, 10)));
+		ctrl_nodes_.push_back(ControlNode::Make(ControlNodeType::kRotate, SkRect::MakeXYWH(selectionBound.fLeft, selectionBound.fTop, 10, 10)));
+	}
+private:
+	Document* document_;
+	std::vector<CtrlNode> ctrl_nodes_;
 };
