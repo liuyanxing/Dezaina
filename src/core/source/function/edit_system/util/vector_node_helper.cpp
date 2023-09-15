070f15f6e85a52765c4fedcc721eeb79b53f1229
liuyanxing <liuyanxing@jwzg.com>
2023-09-15 16:39:46 +0800

    WIP: compute vector geometry

--- src/core/source/function/edit_system/util/vector_node_helper.cpp
+++ src/core/source/function/edit_system/util/vector_node_helper.cpp
@@ -7,29 +7,35 @@
 #include "src/pathops/SkOpEdgeBuilder.h"
 #include "src/pathops/SkPathOpsCommon.h"
 #include "src/pathops/SkAddIntersections.h"
+#include "src/pathops/SkPathWriter.h"
 
 namespace util {
   void networkToSkPath(VectorEditor::Network& network, SkPath& path) {
     for (auto* segment : network.getSegments()) {
       auto& [v0, v1] = segment->getVertecies();
-      auto t0 = v0->getTangentEnd();
-      auto t1 = v1->getTangentEnd();
       path.moveTo(v0->x(), v0->y());
-      path.cubicTo(
-        t0.x, t0.y,
-        t1.x, t1.y,
-        v1->x(), v1->y()
-      );
-    }
+      if (v0->hasTangent() || v1->hasTangent()) {
+        auto t0 = v0->getTangentEnd();
+        auto t1 = v1->getTangentEnd();
+        path.cubicTo(
+          t0.x, t0.y,
+          t1.x, t1.y,
+          v1->x(), v1->y()
+        );
+      } else {
+        path.lineTo(v1->x(), v1->y());
+      }
+   }
   }
 
-  vector<SkPath> computeFillGeometryPath(const SkPath& path) {
+  vector<SkPath> computeFillGeometryPath(SkPath& path) {
+    path.dump();
     SkSTArenaAlloc<4096> allocator;
     SkOpContour contour;
     SkOpContourHead* contourList = static_cast<SkOpContourHead*>(&contour);
     SkOpGlobalState globalState(contourList, &allocator, true, nullptr);
     SkOpCoincidence coincidence(&globalState);
-    SkOpEdgeBuilder builder(path, contourList, &globalState);
+    SkOpEdgeBuilder builder(SkPathWriter(path), contourList, &globalState);
     if (!builder.finish()) {
       return {};
     }
@@ -42,6 +48,12 @@ namespace util {
       while (AddIntersectTs(current, next, &coincidence)
               && (next = next->next()));
     } while ((current = current->next()));
+
+    bool success = HandleCoincidence(contourList, &coincidence);
+    if (!success) {
+      return {};
+    }
+
     return {};
   }
 }
