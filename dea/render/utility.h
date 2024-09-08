80fa7c2676736360add311c08607efdbd0762535
liuyanxing <lyx2049@qq.com>
2024-09-08 22:29:01 +0800

    fix render utility

--- dea/render/utility.h
+++ dea/render/utility.h
@@ -5,14 +5,33 @@
 #include "node.h"
 
 namespace dea::render {
-inline SkColor toSkColor(const node::Color &color) {
-  return SkColorSetARGB(color.a, color.r, color.g, color.b);
+
+inline SkMatrix toSkMatrix(const node::Matrix& matrix) {
+	return SkMatrix::MakeAll(
+		matrix.m00, matrix.m01, matrix.m02,
+		matrix.m10, matrix.m11, matrix.m12,
+		0, 0, 1
+	);
+}
+inline node::Matrix toMatrix(const SkMatrix& matrix) {
+	return node::Matrix{
+		matrix.get(0),
+		matrix.get(1),
+		matrix.get(2),
+		matrix.get(3),
+		matrix.get(4),
+		matrix.get(5),
+	};
 }
 
-inline SkMatrix toSkMatrix(const node::Matrix &matrix) {
-	SkMatrix skMatrix;
-	skMatrix.setAll(matrix.m00, matrix.m01, matrix.m02, matrix.m10, matrix.m11, matrix.m12, 0, 0, 1);
-	return skMatrix;
+inline SkColor toSkColor(const node::Color& color) {
+	SkColor4f skColorf{
+		color.r,
+		color.g,
+		color.b,
+		color.a,
+	};
+	return skColorf.toSkColor();
 }
 
 } // namespace dea::render
