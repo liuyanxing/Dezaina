f9d117cd1e767038d4a79143389c1be7acc1768a
liuyanxing <1464062049@qq.com>
2024-10-14 20:38:52 +0800

    fix gl

--- app/native/xmake.lua
+++ app/native/xmake.lua
@@ -3,8 +3,17 @@ add_requires("imgui v1.89.4", { configs = { sdl2 = true, opengl3 = true } })
 add_requires("zlib", "minizip")
 
 target("app")
-set_kind("binary")
-add_files("src/*.cpp")
-add_deps("dea")
-add_packages("libsdl", "imgui", "zlib", "minizip")
-set_rundir("$(projectdir)")
+	set_kind("binary")
+	add_files("src/*.cpp")
+	add_deps("dea")
+	add_packages("libsdl", "imgui", "zlib", "minizip")
+	set_rundir("$(projectdir)")
+	if is_plat("windows") then
+			add_syslinks("gdi32", "user32", "opengl32")
+	elseif is_plat("macosx") then
+			add_frameworks("CoreFoundation", "CoreGraphics", "CoreText", "CoreServices")
+	elseif is_plat("linux") then
+			add_deps("fontconfig", "freetype >=2.10")
+			add_syslinks("pthread", "GL", "dl", "rt")
+	end
+target_end()
